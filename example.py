9be835630f3c332252caa17921dd173129b9b03c
judithb97 <55704299+judithb97@users.noreply.github.com>
2021-04-01 11:09:15 +0200

    add factorial test

--- example.py
+++ example.py
@@ -14,3 +14,17 @@ def subtract(a, b):
 def test_subtract():
     assert subtract(2, 3) == -1
 
+#1
+def factorial(n):
+    """
+    Computes the factorial of n.
+    """
+    if n < 0:
+        raise ValueError('received negative input')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+def test_factorial():
+    assert abs(factorial(5) - 5*4*3*2*1) < 1e-6
